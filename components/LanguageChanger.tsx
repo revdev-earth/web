4f2ae53fdbbf3f06745e1744eaeab52a28c0c68b
revione <revione@outlook.com>
2024-03-22 12:13:58 +0100

    Lenguages

--- components/LanguageChanger.tsx
+++ components/LanguageChanger.tsx
@@ -4,7 +4,21 @@ import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 import { useTranslation } from 'react-i18next';
 import i18nConfig from '@/i18nConfig';
-import { ChangeEvent } from 'react';
+import { useState } from 'react';
+import Select, { ActionMeta, SingleValue } from 'react-select';
+
+type OnChangeSelect =
+  | ((
+      newValue: SingleValue<{
+        value: string;
+        label: string;
+      }>,
+      actionMeta: ActionMeta<{
+        value: string;
+        label: string;
+      }>
+    ) => void)
+  | undefined;
 
 export default function LanguageChanger() {
   const { i18n } = useTranslation();
@@ -12,24 +26,42 @@ export default function LanguageChanger() {
   const router = useRouter();
   const currentPathname = usePathname();
 
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const newLocale = e.target.value;
+  const [selectedOption, setSelectedOption] = useState({
+    value: currentLocale,
+    label: getLanguageLabel(currentLocale)
+  });
+
+  const languageOptions = [
+    { value: 'en', label: 'English' },
+    { value: 'de', label: 'Deutsch' },
+    { value: 'ja', label: '日本語' },
+    { value: 'es', label: 'Español' }
+  ];
+
+  const handleChange = (
+    selectedOption: SingleValue<{ value: string; label: string }>
+  ) => {
+    if (!selectedOption) return;
+
+    setSelectedOption(selectedOption);
 
     // set cookie for next-i18n-router
     const days = 30;
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `NEXT_LOCALE=${newLocale};expires=${date.toUTCString()};path=/`;
+    document.cookie = `NEXT_LOCALE=${
+      selectedOption.value
+    };expires=${date.toUTCString()};path=/`;
 
     // redirect to the new locale path
     if (
       currentLocale === i18nConfig.defaultLocale &&
       !i18nConfig.prefixDefault
     ) {
-      router.push('/' + newLocale + currentPathname);
+      router.push('/' + selectedOption.value + currentPathname);
     } else {
       router.push(
-        currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
+        currentPathname.replace(`/${currentLocale}`, `/${selectedOption.value}`)
       );
     }
 
@@ -37,14 +69,65 @@ export default function LanguageChanger() {
   };
 
   return (
-    <select
+    <Select
+      options={languageOptions}
       onChange={handleChange}
-      value={currentLocale}
-      className="p-2 border rounded-md bg-transparent">
-      <option value="en">English</option>
-      <option value="de">Deutsch</option>
-      <option value="ja">日本語</option>
-      <option value="es">Español</option>
-    </select>
+      value={selectedOption}
+      styles={{
+        control: baseStyles => ({
+          ...baseStyles,
+          border: 0,
+          boxShadow: '0 0 0',
+          background: '#e1e6ec90',
+          cursor: 'pointer'
+        }),
+        input: b => ({ ...b, minWidth: '80px', cursor: 'pointer' }),
+        singleValue: b => ({
+          ...b,
+          textAlign: 'center',
+          color: '#000000c9',
+          cursor: 'pointer'
+        }),
+        indicatorsContainer: () => ({ display: 'none' }),
+        menu: b => ({
+          ...b,
+          marginTop: '1px',
+          background: '#e1e6ec90',
+          width: '100px',
+          cursor: 'pointer',
+          borderRadius: '3px'
+        }),
+        menuList: b => ({
+          ...b,
+          padding: '0'
+        }),
+        option: (b, s) => ({
+          ...b,
+          cursor: 'pointer',
+          textAlign: 'center',
+          color: s.isSelected || s.isFocused ? '#e1e6ec' : '#000000c9',
+          background: s.isSelected
+            ? '#000000c9'
+            : s.isFocused
+            ? '#00000059'
+            : '#e1e6ecc9'
+        })
+      }}
+    />
   );
+}
+
+function getLanguageLabel(locale: string) {
+  switch (locale) {
+    case 'en':
+      return 'English';
+    case 'de':
+      return 'Deutsch';
+    case 'ja':
+      return '日本語';
+    case 'es':
+      return 'Español';
+    default:
+      return '';
+  }
 }
